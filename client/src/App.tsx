c6813ef67a4d6ffa4a74da42a514c2cec376d022
Tayte555 <tayte2004@gmail.com>
2026-02-24 16:18:15 +0000

    ATH-35: protect routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from "react-router-dom";
+import ProtectedRoute from "./components/ProtectedRoute";
 import Landing from "./pages/landing/LandingPage";
 import Register from "./pages/registration/RegistrationPage";
 import Login from "./pages/login/LoginPage";
@@ -10,7 +11,14 @@ export default function App() {
       <Route path="/" element={<Landing />} />
       <Route path="/register" element={<Register />} />
       <Route path="/login" element={<Login />} />
-      <Route path="/dashboard" element={<Dashboard />} />
+      <Route
+        path="/dashboard"
+        element={
+          <ProtectedRoute>
+            <Dashboard />
+          </ProtectedRoute>
+        }
+      />
     </Routes>
   );
 }
